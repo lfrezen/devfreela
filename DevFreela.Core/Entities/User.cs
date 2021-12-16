0984ea0e8e59579cb805919aaf7356b331061900
Lucian Rezende <lucianfiocello@gmail.com>
2021-12-16 11:27:51 -0300

    Modelando a camada de domínio

---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullname, string email, DateTime birthdate)
+        {
+            Fullname = fullname;
+            Email = email;
+            Birthdate = birthdate;
+            CreatedAt = DateTime.Now;
+            Active = true;
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+        }
+
+        public string Fullname { get; private set; }
+        public string Email { get; private set; }
+        public DateTime Birthdate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProjects { get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+    }
+}
